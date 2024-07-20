912f415d728eb0584b530181241d2bc108266903
Faris <farissharayri@gmail.com>
2024-07-20 18:35:12 -0400

    boilerplate

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+//initial boilerplate for the game, this will be modified as the game is being built.
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player pressing a button with a `buttonPressHandle` function.
+
+//7) Create Reset functionality.
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
